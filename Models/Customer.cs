b580c2403efa56ff1eb12faf246847164a8a9907
Joseph Kelley <josephkelley91@gmail.com>
2025-01-11 09:38:42 -0500

    Modified Customer model to match expected data types and keys.

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -2,11 +2,11 @@ namespace PlushiesRUs.Models;
 
 public class Customer
 {
-    public int Id { get; set; }
+    public short Id { get; set; }
 
-    public string Name { get; set; }
+    public string Full_Name { get; set; }
 
     public string Email { get; set; }
 
-    public string FirstPurchaseDate { get; set; }
+    public DateTime First_Purchase_Date { get; set; }
 }
